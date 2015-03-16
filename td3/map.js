33f982722f6b8bcfc6ce12b85bd6185f5531d177
Pacome Bondet de La Bernardie <pabondet@gmail.com>
2015-03-16 11:28:58 -0400

    reussie a dropper un pin sur google maps

--- td3/map.js
+++ td3/map.js
@@ -24,8 +24,46 @@
 	});
 
 	$("#btnRecherche").click(function(){
+		dropPin();
 	});
 
 	$("#monBouton").click(function(){
 	});
+
+
+				
+	var mapOptions = {
+		zoom: 4,
+		center: new google.maps.LatLng(46.8167, -71.2167)
+	};
+
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+					mapOptions);
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+					'&signed_in=true&callback=initialize';
+	document.body.appendChild(script);
+				
+	function dropPin() {
+  		var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  		var mapOptions = {
+   	 		zoom: 6,
+   	 		center: myLatlng
+  		}
+ 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  		var marker = new google.maps.Marker({
+  			position: myLatlng,
+    	  	map: map,
+     	 	title: 'Hello World!'
+  	});
+}
+
+	//google.maps.event.addDomListener(window, 'load', initialize);
+	window.onload = loadScript;
+
+
+
+			
 });
