fc0a31adfa4d0a50af10ea7e2645d50a523b2c50
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2015-03-16 08:41:47 -0400

    td3 init

---
+++ td3/map.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function(){
+	// Ecrivez ici tout le code qui doit s'éxecuter lorsque
+	// la page est complètement chargée
+	
+	// $ est un raccourci pour jQuery
+	// A l'aide de $, vous pouvez chercher n'importe quel
+	// element de la page. Si celui-ci contient un identifiant
+	// unique #identifiant vous permet de le retrouver d'un 
+	// seul coup
+	
+	// La methode .click attache une fonction de rappel à tous 
+	// les objets selectionnés avec $("#monBouton"), donc dans 
+	// ce cas, le seul bouton sur la page, mais notez que vous
+	// pouvez appliquer une fonction de rappel sur plusieurs 
+	// éléments en même temps.
+	$("#monBouton").click(function(){
+		alert("Vous venez de cliquer le bouton!");
+		
+		// Ceci cache tous les paragraphes sur la page
+		$("p").hide();
+	});
+});
